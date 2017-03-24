3ef48aeac85d03b78fb826c96b03133f3119079f
abdullah samman <sammanabdallah@gmail.com>
2017-03-24 05:05:31 +0000

    add gitlab tively workflow /spend 1h40m

--- app.tivly.js
+++ app.tivly.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express()
 var request = require('request')
-var curlify = require('request-as-curl')
 var util = require('util')
-var async = require('async')
+var fs = require('fs');
 
+const GITLAB_CI_YML_CONTENT = fs.readFileSync('gitlab-pipeline/gitlab-ci.yml').toString('base64')
 const REDIRECT_URL = "http://bahama-agenda-3000.codio.io/oauth-gitlab"
 const CLIENT_SECRET = "b713ec666614a6f06ae2c15d06117cf8d76a33398fa9108e69653665b7f5845b"
 const CLIENT_ID = "6b7979bc2dbd78444592e30d624aa37cc0fc2c9145f3af44f1f70f5c4adbf21d"
@@ -22,12 +22,12 @@ var make_api_set_status_req = function(owner, repo_name, commit_sha, data){
   }
 }
 
-var make_api_get_req = function(url){
+var make_api_get_req = function(url, token){
   return {
     method: "GET",
     url: url,
     headers: {
-        'Authorization': "token " + GITHUB_TOKEN,
+        'Authorization': "Bearer " + token,
         'User-Agent': 'Tivly 1.0 Beta'
     },
   }
@@ -46,15 +46,64 @@ var execute_on_full_recieve = function(stream, callback){
 var main = function(url, req, res){
   if (url == '/oauth-gitlab'){
     params = req.query
-//     data = util.format("client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=%s", CLIENT_ID, CLIENT_SECRET, params.code, REDIRECT_URL)
-    data = "client_id=6b7979bc2dbd78444592e30d624aa37cc0fc2c9145f3af44f1f70f5c4adbf21d&client_secret=b713ec666614a6f06ae2c15d06117cf8d76a33398fa9108e69653665b7f5845b&code="+params.code+"&grant_type=authorization_code&redirect_uri=http://bahama-agenda-3000.codio.io/oauth-gitlab"
+    data = util.format("client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=%s", CLIENT_ID, CLIENT_SECRET, params.code, REDIRECT_URL)
+    console.log('INFO: got request containing code to get access_token')
     request({
     	method: 'POST',
-	url: 'https://gitlab.com/oauth/token',
-        headers: {'User-Agent': 'Tively'},
-	data: data 
+    	url: 'https://gitlab.com/oauth/token',
+	    body: data 
     }, function(error, response, body){
-      res.send( data + body )
+        if(error){
+          console.log('ERROR: couldn\'t get access_token')
+        }else{
+          body = JSON.parse(body)
+          request(
+            make_api_get_req('https://gitlab.com/api/v4/projects?owned=true', body["access_token"]),
+            function(err, resp, projs_body){
+              if(err){
+                console.log('ERROR: can\'t get projects owned by user')
+              }else{
+                projs_body = JSON.parse(projs_body)
+                console.log('INFO: got projects owned by user... rendering')
+                res.render('choose-project', {token: body["access_token"], projs_body: projs_body})
+              }
+            }
+          )
+        }
+    })
+  }else if(url == '/hook-project'){
+    params = req.query
+    console.log('INFO: got request to hook project')
+    request({
+        method: "PUT",
+        url: "https://gitlab.com/api/v4/projects/"+params.proj_id,
+        headers: {
+          'Authorization': "Bearer " + params.access_token,
+          'User-Agent': 'Tivly 1.0 Beta'
+        },
+        body: {only_allow_merge_if_pipeline_succeeds: true},
+        json: true
+    }, function(error, response, body){
+        if (error){
+            console.log('ERROR: can\'t update project settings to not merge unless pipelines succeed')
+        }else{
+            console.log('INFO: changed project settings to not merge unless pipelines succeed')
+            request({
+                method: "POST",
+                headers: {
+                  'Authorization': "Bearer " + params.access_token,
+                  'User-Agent': 'Tivly 1.0 Beta'
+                },
+                url: "https://gitlab.com/api/v4/projects/"+params.proj_id+"/repository/files/.gitlab-ci.yml?branch=master&content="+ GITLAB_CI_YML_CONTENT +"&commit_message=Setup%20Tivly&encoding=base64"
+            }, function(err, resp, add_hook_body){
+                if (err){
+                    console.log("ERROR: couldn't create .gitlab-ci.yml pipeline config")
+                }else{
+                    console.log("SUCCESS: created .gitlab-ci.yml pipeline config file")
+                    res.send('SUCCESS')
+                }
+            })
+        }
     })
   }
 }
@@ -62,4 +111,11 @@ var main = function(url, req, res){
 app.get('/oauth-gitlab', function(req, res) {
   main('/oauth-gitlab', req, res)
 })
+
+app.get('/hook-project', function(req, res){
+  main('/hook-project', req, res)
+})
+
+app.set('view engine', 'pug')
+app.set('views', 'views')
 app.listen(3000)
